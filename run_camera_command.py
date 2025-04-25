f33985252b9543e29155279a7d25c1e592acce0b
Jin Minu <s9430939@naver.com>
2025-04-26 02:31:32 +0900

    프레임드랍 정확도에 따라서 다름

--- run_camera_command.py
+++ run_camera_command.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python3
+"""
+프로그램 실행시,
+python run_camera_command.py 2>/dev/null (shell파일로 만들것)
+GStreamer관련 출력들 차단함
+"""
+
 import sys, os, gi, re
 from gi.repository import Gst, GLib
 
@@ -12,15 +17,16 @@ Gst.init(None)
 
 # 전체 영상에 대해 상하 반전을 inference + display 양쪽에 모두 적용하기 위해
 # tee 이전에 qtivtransform flip-vertical=true 삽입
+# threshold에 따라서 프레임 드랍 현상 생김 -> 조정 필요
 pipeline_str = (
     'qtiqmmfsrc camera=0 ! '
     'qtivtransform flip-vertical=true ! '
     'video/x-raw(memory:GBM),format=NV12,width=1920,height=1080,framerate=30/1 ! '
     'tee name=t '
-    't. ! queue ! qtimetamux name=mux ! qtioverlay ! waylandsink sync=false '
+    't. ! queue ! qtimetamux name=mux ! qtioverlay ! waylandsink fullscreen=true sync=false '
     't. ! queue ! qtimlvconverter ! '
     'qtimlsnpe delegate=dsp model=/opt/yolonas.dlc layers="</heads/Mul,/heads/Sigmoid>" ! '
-    'qtimlvdetection module=yolo-nas labels=/opt/yolonas.labels threshold=71.0 results=10 ! '
+    'qtimlvdetection module=yolo-nas labels=/opt/yolonas.labels threshold=91.0 results=10 ! '
     'text/x-raw ! tee name=mt '
     'mt. ! queue ! mux. '
     'mt. ! queue ! appsink name=meta_sink emit-signals=true sync=false drop=true max-buffers=1'
