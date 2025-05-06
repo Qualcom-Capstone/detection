9e9defc7135a452c2b826e7a61eae85c3e768fc1
Jin Minu <s9430939@naver.com>
2025-05-07 02:18:12 +0900

    모델 교체->프레임완화

--- pipeline_config/pipeline_config.py
+++ pipeline_config/pipeline_config.py
@@ -3,21 +3,25 @@
 # threshold에 따라서 프레임 드랍 현상 생김 -> 조정 필요
 
 pipeline_str = (
-    'qtiqmmfsrc camera=0 ! '
-    'qtivtransform flip-vertical=true ! '
+    'qtiqmmfsrc name=camsrc camera=0 ! '
     'video/x-raw(memory:GBM),format=NV12,width=1920,height=1080,framerate=30/1 ! '
-    'tee name=t '
-    't. ! queue ! qtimetamux name=mux ! qtioverlay ! waylandsink fullscreen=true sync=false '
-    't. ! queue ! qtimlvconverter ! '
-    'qtimlsnpe delegate=dsp model=/opt/yolonas.dlc layers="</heads/Mul,/heads/Sigmoid>" ! '
-    'qtimlvdetection module=yolo-nas labels=/opt/yolonas.labels threshold=71.0 results=10 ! '
+    'queue ! tee name=split '
+    'split. ! queue ! '
+    'qtimetamux name=metamux ! queue ! '
+    'qtioverlay ! queue ! '
+    'waylandsink sync=false fullscreen=true '
+    'split. ! queue ! qtimlvconverter ! queue ! '
+    'qtimltflite delegate=external external-delegate-path=libQnnTFLiteDelegate.so '
+      'external-delegate-options="QNNExternalDelegate,backend_type=htp;" '
+      'model=/opt/yolov5m-320x320-int8.tflite ! queue ! '
+    'qtimlvdetection threshold=75.0 results=10 module=yolov5 '
+      'labels=/opt/yolov5m.labels '
+      'constants="YoloV5,q-offsets=<3.0>,q-scales=<0.005047998391091824>;" ! '
     'text/x-raw ! tee name=mt '
-    'mt. ! queue ! mux. '
+    'mt. ! queue ! metamux. '
     'mt. ! queue ! appsink name=meta_sink emit-signals=true sync=false drop=true max-buffers=1 '
-    't. ! queue ! qtivtransform ! videoconvert ! '
-    'video/x-raw,format=RGB,width=1920,height=1080 ! '
-    'appsink name=frame_sink emit-signals=false sync=false max-buffers=1 drop=true'
 )
+
 
 
 def get_pipeline():
