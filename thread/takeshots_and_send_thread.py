25efb9e8dae7e3217670c285d851c6e87acae2c8
Jin Minu <s9430939@naver.com>
2025-05-17 01:22:48 +0900

    정보 전송 print문

--- thread/takeshots_and_send_thread.py
+++ thread/takeshots_and_send_thread.py
@@ -23,6 +23,11 @@ def save_and_send(frame_sink):
             img_path = f"/home/root/detection/images/screenshot_{meta_item['id']}.jpg"
             s3_upload.upload_image_to_cars_folder(img_path)  # 찍은 이미지 s3서버로 전송
             # send_to_server(meta_item) # 메타정보 서버로 보냄
+            print("--------------------------------------------")
+            print(f"ID: {meta_item['id']} 차량 속도 위반")
+            print(f"screenshot_{meta_item['id']} 서버 전송 완료.")
+            print("메타 정보 서버 전송 완료.")
+            print("--------------------------------------------")
 
         except Exception as e:
             print(f"Save and Send Thread Error! : {e}")
