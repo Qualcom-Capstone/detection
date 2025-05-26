4dbf2d91ceeb71b7aa461cc7b17a3bd0f7469879
Jin Minu <s9430939@naver.com>
2025-05-26 09:28:36 +0900

    Coordinate 객체 수정

--- coords/Coordinate.py
+++ coords/Coordinate.py
@@ -5,18 +5,20 @@
 
 class Coordinate:
     def __init__(self, x: float, y: float, w: float, h: float):
-        self.x = x  # 중심 x (상대 위치임)
-        self.y = y  # 중심 y
-        self.w = w  # 너비
-        self.h = h  # 높이
+        self.x = x  # 좌측 상단 x좌표
+        self.y = y  # 좌측 상단 y좌표
+        self.w = w  # 좌측 상단에서 가로 길이
+        self.h = h  # 좌측 상단에서 부터의 세로 길이
 
     # 바운딩박스 중심좌표 반환
     def center(self):
-        return self.x, self.y
+        center_x = self.x + self.w / 2
+        center_y = self.y + self.h / 2
+        return center_x, center_y
 
     # 하단 좌표 반환
     def bottom(self):
-        return self.y + self.h / 2
+        return self.y + self.h
 
     # 바운딩박스 면적 반환
     def area(self):
